4e8f5d7c59fcc0fac37f28e891a1649028cedd6d
Bazuka <suradaniil74@gmail.com>
2026-05-12 16:14:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 # Пробуем HTTP (а не HTTPS)
 try:
-    response = requests.get('http://t-e-t-o.ct.ws/ping.php', timeout=10)
+    response = requests.get('dsfdgffdg.atwebpages.com/ping.php', timeout=10)
     print(response.text)
 except Exception as e:
     print(f"Ошибка: {e}")
