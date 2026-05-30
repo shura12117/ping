d1432da7c956013cbcfca92ddfddf95a899e9e1b
Bazuka <suradaniil74@gmail.com>
2026-05-30 14:01:04 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# Пробуем HTTP (а не HTTPS)
-try:
-    response = requests.get('dsfdgffdg.atwebpages.com/ping.php', timeout=10)
-    print(response.text)
-except Exception as e:
-    print(f"Ошибка: {e}")
